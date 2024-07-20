ced6dd93f355b19215c75b9636a474deefafd09e
Roberto <p.roberto@wustl.edu>
2024-07-20 15:13:22 -0700

    fixing install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# Check python3 version
-# if ! 3 -c 'import sys; assert sys.version_info >= (3,11)' > /dev/null; then
-#     echo "python3 version should be >= 3.11"
-#     exit 1
-# fi
+# Check Python version
+if ! python -c 'import sys; assert sys.version_info >= (3,11)' > /dev/null; then
+    echo "Python version should be >= 3.11"
+    exit 1
+fi
 
-# Update python3 resources at the global level
-python3 -m pip install --upgrade setuptools wheel pip
+# Update Python resources at the global level
+python -m pip install --upgrade setuptools wheel pip
 
 # Setup and activate virtual environment
-python3 -m venv .venv
+python -m venv .venv
 source .venv/bin/activate
 if [ $? -ne 0 ]; then
     echo "Failed to activate the virtual environment."
     exit 1
 fi
 
-python3 -m pip install --upgrade setuptools wheel pip
+python -m pip install --upgrade setuptools wheel pip
 
 # Check and install requirements
 if [ -f "requires/requirements.txt" ]; then
-    python3 -m pip install -r requires/requirements.txt
+    python -m pip install -r requires/requirements.txt
 fi
 
 if [ -f "requires/dev.txt" ]; then
-    python3 -m pip install -r requires/dev.txt
+    python -m pip install -r requires/dev.txt
 fi
